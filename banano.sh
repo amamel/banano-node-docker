6ca86242fb2fbcdfab2a3408c2eb183368c3bf83
Alex Mamel <1643277+amamel@users.noreply.github.com>
2023-06-24 15:05:32 -0400

    more

--- banano.sh
+++ banano.sh
@@ -4,4 +4,4 @@ echo "== Cloning installation"
 git -C /opt/banano-node-docker pull || git clone https://github.com/amamel/banano-node-docker.git /opt/banano-node-docker
 
 echo "== Starting installation"
-sudo bash /opt/banano-node-docker/bnd/banano.sh -s
+sudo bash /opt/banano-node-docker/bnd/install.sh -s
